dd111b64bbb7303a3f945a99544fc87880b95b43
rabunjauh <mustafa.visionet@gmail.com>
2020-09-06 21:30:50 +0700

    update

--- pattern.php
+++ pattern.php
@@ -17,31 +17,47 @@
 
 // ----------------------------
 
-for($i=1;$i <= 10; $i++){
-	for($j = 10; $j > $i; $j--){
-		echo "&nbsp;&nbsp;";
+// for($i=1;$i <= 10; $i++){
+// 	for($j = 10; $j > $i; $j--){
+// 		echo "&nbsp;&nbsp;";
+// 	}
+
+// 	for($k = 1; $k <= (2 * $i - 1); $k++){
+// 		echo "*";
+// 	}
+
+// 	echo "<br>";
+// }
+
+// for($i=2;$i <= 10; $i++){
+// 	for($j = 1; $j < $i; $j++){
+// 		echo "&nbsp;&nbsp;";
+// 	}
+
+// 	for($k = 10; $k >= (2 * $i - 10); $k--){
+// 		echo "*";
+// 	}
+
+// 	// for($k = 1; $k < $i; $k++){
+// 	// 	echo "0";
+// 	// }
+
+// 	echo "<br>";
+// }
+
+// formula piramid 
+// n = rows
+// column = 2n - 1
+
+$n = 50;
+for($i = 1;$i <= $n; $i++){
+	for($j = 1; $j <= (2 * $n)-1; $j++){
+		if($j >= $n-($i-1) && $j <= $n+($i-1)){
+			echo "*";
+		}else{
+			echo "&nbsp;&nbsp;";
+		}
 	}
-
-	for($k = 1; $k <= (2 * $i - 1); $k++){
-		echo "*";
-	}
-
-	echo "<br>";
-}
-
-for($i=2;$i <= 10; $i++){
-	for($j = 1; $j < $i; $j++){
-		echo "&nbsp;&nbsp;";
-	}
-
-	for($k = 10; $k >= (2 * $i - 10); $k--){
-		echo "*";
-	}
-
-	// for($k = 1; $k < $i; $k++){
-	// 	echo "0";
-	// }
-
 	echo "<br>";
 }
 
--- random.php
+++ random.php
@@ -41,19 +41,39 @@
 // 	$a = $hasil;
 // }
 
+// $a = 1;
+// echo $a . " ";
+// for($i=1;$i<9;$i++){
+// 	$penambah = $i * 2;
+// 	echo $hasil = $a + $penambah;
+// 	echo " ";
+// 	$a = $hasil;
+// }
+
 // hasil 3 7 13 21 31 43 57 73
 
 // ------------------------------------------
 
 // Soal : Menampilkan 1+2+3+4+5 = 15, inputan = 15
 
-for($i = 1; $i <=15;$i++){
-	if($i < 15){
-		echo $i."+";
-	}else{
-		echo $i ."=";
-	}
+// for($i = 1; $i <=15;$i++){
+// 	if($i < 15){
+// 		echo $i."+";
+// 	}else{
+// 		echo $i ."=";
+// 	}
+	
+// }
 
+for($i = 0; $i < 10; $i++){
+	for($j = 1; $j < 10; $j++){
+		if ($j == 5){
+			echo "*";
+		}else{
+			echo "#";
+		}
+	}
+	echo "<br>";
 }
 
 
